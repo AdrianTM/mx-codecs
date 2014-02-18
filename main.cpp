298ea0c68be4a1b15a85fbda8b4dcc20af960e70
anticapitalista <antiX@operamail.com>
2014-02-18 13:40:35 +0200

    Update main.cpp
    Added code for qt localisation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <qtranslator.h>
+#include <qlocale.h>
 #include "mxcodecs.h"
 #include <unistd.h>
 
@@ -8,6 +10,14 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   a.setWindowIcon(QIcon("/usr/share/icons/Tango/16x16/mimetypes/sound.png"));
+  
+  QTranslator qtTran;
+  qtTran.load(QString("qt_") + QLocale::system().name());
+  a.installTranslator(&qtTran);
+  
+  QTranslator appTran;
+  appTran.load(QString("mx-codecs_") + QLocale::system().name(), "/usr/share/mx-codecs/locale");
+  a.installTranslator(&appTran);
 
 
   if (getuid() == 0) {
