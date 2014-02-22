2ff72472c69cf06a83fffeacff34abc932c750b4
AdrianTM <AdrianTM@users.noreply.github.com>
2014-02-22 12:56:28 -0500

    debianized source

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
+/*****************************************************************************
+ * main.cpp
+ *****************************************************************************
+ * Copyright (C) 2014 MX Authors
+ * 
+ * Authors: Jerry 3904
+ *          Anticaptilista
+ *          Adrian
+ *          MEPIS Community <http://forum.mepiscommunity.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+
 #include <QApplication>
 #include <qtranslator.h>
 #include <qlocale.h>
