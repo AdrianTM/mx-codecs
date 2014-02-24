eafa7d704270895d6213d81c9c2964b714aec36f
anticapitalista <antiX@operamail.com>
2014-02-24 17:57:07 +0200

    fully translated .qm and .ts files

--- translations/mx-codecs_ro.ts
+++ translations/mx-codecs_ro.ts
@@ -1,10 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ro" version="2.0">
 <context>
     <name>QApplication</name>
     <message>
         <location filename="main.cpp" line="32"/>
         <source>You must run this program as root.</source>
-        <translation>U dient deze toepassing als &apos;root&apos; uit te voeren.</translation>
+        <translation>Trebuie să rulați programul ca administrator.</translation>
     </message>
 </context>
 <context>
@@ -19,25 +19,25 @@
         <location filename="mxcodecs.ui" line="33"/>
         <location filename="ui_mxcodecs.h" line="174"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;This application allows you to install restricted codecs that permit advanced video and audio functions.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;In some juridictions their distribution may be limited so the user must meet local regulations. &lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Do you assume legal responsibility for downloading these codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;Deze applicatie staat u toe gelimiteerde codecs te gebruiken die geavanceerde video en audio functies toestaan.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;In sommige jurisdicties kan hun distributie beperkt zijn zodat de gebruiker de lokale regels naleeft.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Neemt u de legale verantwoordelijkheid tot het downloaden van deze codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;Aceast program poate fi folosit pentru descărcarea și instalarea de codecuri multimedia care permit utilizare unor funcții avansate video și audio.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;În anumite jurisdicții distribuirea acestor codecuri poate fi restrictionată, utilizatorul programului trebuie să respecte legile locale. &lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Vă asumați responsabilitatea pentru descărcarea acestor codecuri?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="48"/>
         <location filename="ui_mxcodecs.h" line="175"/>
         <source>Downloading codecs files</source>
-        <translation>Codecs bestanden downloaden</translation>
+        <translation>Descărcare fișiere codec</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="93"/>
         <location filename="ui_mxcodecs.h" line="178"/>
         <source>Cancel any changes then quit</source>
-        <translation>Iedere verandering ongedaan maken, dan afsluiten</translation>
+        <translation>Anulează toate schimbările și închide programul</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="96"/>
         <location filename="ui_mxcodecs.h" line="180"/>
         <source>Cancel</source>
-        <translation>Annuleren</translation>
+        <translation>Anulează</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="103"/>
@@ -49,7 +49,7 @@
         <location filename="mxcodecs.ui" line="113"/>
         <location filename="ui_mxcodecs.h" line="183"/>
         <source>Apply any changes then quit</source>
-        <translation>Veranderingen doorvoeren en afsluiten</translation>
+        <translation>Aplică toate schimbările și închide programul</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="116"/>
@@ -61,13 +61,13 @@
         <location filename="mxcodecs.ui" line="171"/>
         <location filename="ui_mxcodecs.h" line="189"/>
         <source>About this application</source>
-        <translation>Over deze toepassing</translation>
+        <translation>Informații despre program</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="174"/>
         <location filename="ui_mxcodecs.h" line="191"/>
         <source>About...</source>
-        <translation>Over...</translation>
+        <translation>Despre...</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="181"/>
@@ -79,13 +79,13 @@
         <location filename="mxcodecs.ui" line="210"/>
         <location filename="ui_mxcodecs.h" line="194"/>
         <source>Display help </source>
-        <translation>Toon help</translation>
+        <translation>Afișează ajutor</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="213"/>
         <location filename="ui_mxcodecs.h" line="196"/>
         <source>Help</source>
-        <translation>Help</translation>
+        <translation>Ajutor</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="220"/>
@@ -99,74 +99,74 @@
         <location filename="mxcodecs.cpp" line="106"/>
         <location filename="mxcodecs.cpp" line="113"/>
         <source>&lt;b&gt;Running command...&lt;/b&gt;&lt;p&gt;</source>
-        <translation>&lt;b&gt;Commando uitvoeren...&lt;/b&gt;&lt;p&gt;</translation>
+        <translation>&lt;b&gt;Se rulează comanda...&lt;/b&gt;&lt;p&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="95"/>
         <location filename="mxcodecs.cpp" line="110"/>
         <source>Cannot connect to the download site</source>
-        <translation>Kan geen verbinding maken met de download site</translation>
+        <translation>Nu se poate conecta la site-ul de descărcare</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="101"/>
         <location filename="mxcodecs.cpp" line="116"/>
         <source>Error downloading %1</source>
-        <translation>Download fout %1</translation>
+        <translation>Eroare descărcare %1</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="120"/>
         <source>&lt;b&gt;Download Finished.&lt;/b&gt;</source>
-        <translation>Download beëindigd.</translation>
+        <translation>S-a terminat descărcarea. </translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="139"/>
         <source>Installing downloaded files</source>
-        <translation>Gedownloade files installeren</translation>
+        <translation>Se instalează fișierele descărcate</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="144"/>
         <source>No downloaded *.debs files found.</source>
-        <translation>Geen gedownloade *.deb bestanden gevonden.</translation>
+        <translation>Nu s-a găsit nici un fisier *.deb descărcat.</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="151"/>
         <source>&lt;b&gt;Installing...&lt;/b&gt;&lt;p&gt;</source>
-        <translation>Installeren...</translation>
+        <translation>&lt;b&gt;Se instalează...&lt;/b&gt;&lt;p&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="154"/>
         <source>Error installing %1</source>
-        <translation>Fout bij installeren %1</translation>
+        <translation>Eroare instalare %1</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="162"/>
         <source>&lt;b&gt;Installation process has finished&lt;/b&gt;</source>
-        <translation>Installatieproces beëindigd</translation>
+        <translation>&lt;b&gt;S-a terminat instalarea&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="166"/>
         <source>Codecs files have been downloaded and installed successfully.</source>
-        <translation>De codecs bestanden zijn gedownload en succesvol geïnstalleerd.</translation>
+        <translation>Fișierele codec au fost descărcate si instalate cu succes.</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="170"/>
         <source>Process finished. Errors have occurred during the installation.</source>
-        <translation>Proces beëindigd. Er hebben zich fouten voorgedaan tijdens de installatie.</translation>
+        <translation>Actiune terminată. Au fost detectate erori in timpul instalării. </translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="182"/>
         <source>About mx-codecs</source>
-        <translation>Over mx-codecs</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="183"/>
         <source>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;&lt;h2&gt;MX Codecs Installer&lt;/h2&gt;&lt;/b&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;MX14+git20140217&lt;/p&gt;&lt;p&gt;&lt;h3&gt;Simple codecs      downloader for antiX MX&lt;/h3&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;http://www.mepiscommunity.org/mx&quot;&gt;      http://www.mepiscommunity.org/mx&lt;/a&gt;&lt;br /&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Copyright (c) antiX&lt;br /&gt;&lt;br /&gt;&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;&lt;h2&gt;MX Codecs Installeerder&lt;/h2&gt;&lt;/b&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;MX14+git20140217&lt;/p&gt;&lt;p&gt;&lt;h3&gt;Eenvoudige codecs downloader voor antiX MX&lt;/h3&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;http://www.mepiscommunity.org/mx&quot;&gt; http://www.mepiscommunity.org/mx&lt;/a&gt;&lt;br /&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Copyright (c) antiX&lt;br /&gt;&lt;br /&gt;&lt;/p&gt;</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="186"/>
         <source>License</source>
-        <translation>Licentie</translation>
+        <translation>Licență</translation>
     </message>
 </context>
 </TS>
--- translations/mx-codecs_sv.ts
+++ translations/mx-codecs_sv.ts
@@ -1,10 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv" version="2.0">
 <context>
     <name>QApplication</name>
     <message>
         <location filename="main.cpp" line="32"/>
         <source>You must run this program as root.</source>
-        <translation>U dient deze toepassing als &apos;root&apos; uit te voeren.</translation>
+        <translation>Du måste köra detta program som root</translation>
     </message>
 </context>
 <context>
@@ -19,25 +19,25 @@
         <location filename="mxcodecs.ui" line="33"/>
         <location filename="ui_mxcodecs.h" line="174"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;This application allows you to install restricted codecs that permit advanced video and audio functions.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;In some juridictions their distribution may be limited so the user must meet local regulations. &lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Do you assume legal responsibility for downloading these codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;Deze applicatie staat u toe gelimiteerde codecs te gebruiken die geavanceerde video en audio functies toestaan.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;In sommige jurisdicties kan hun distributie beperkt zijn zodat de gebruiker de lokale regels naleeft.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Neemt u de legale verantwoordelijkheid tot het downloaden van deze codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;Det här programmet låter dig installera begränsade codecs som tillåter avancerade video och audio funktioner&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;I en del länder kan deras distribution vara begränsad så användaren måste följa lokala lagar.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Tar du lagligt ansvar för att ladda ner dessa codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="48"/>
         <location filename="ui_mxcodecs.h" line="175"/>
         <source>Downloading codecs files</source>
-        <translation>Codecs bestanden downloaden</translation>
+        <translation>Laddar ner codecs-filer</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="93"/>
         <location filename="ui_mxcodecs.h" line="178"/>
         <source>Cancel any changes then quit</source>
-        <translation>Iedere verandering ongedaan maken, dan afsluiten</translation>
+        <translation>Avbryt ändringarna och sedan sluta</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="96"/>
         <location filename="ui_mxcodecs.h" line="180"/>
         <source>Cancel</source>
-        <translation>Annuleren</translation>
+        <translation>Avbryt</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="103"/>
@@ -49,7 +49,7 @@
         <location filename="mxcodecs.ui" line="113"/>
         <location filename="ui_mxcodecs.h" line="183"/>
         <source>Apply any changes then quit</source>
-        <translation>Veranderingen doorvoeren en afsluiten</translation>
+        <translation>Genomför ändringarna och sedan sluta</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="116"/>
@@ -61,13 +61,13 @@
         <location filename="mxcodecs.ui" line="171"/>
         <location filename="ui_mxcodecs.h" line="189"/>
         <source>About this application</source>
-        <translation>Over deze toepassing</translation>
+        <translation>Om detta program</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="174"/>
         <location filename="ui_mxcodecs.h" line="191"/>
         <source>About...</source>
-        <translation>Over...</translation>
+        <translation>Om...</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="181"/>
@@ -79,13 +79,13 @@
         <location filename="mxcodecs.ui" line="210"/>
         <location filename="ui_mxcodecs.h" line="194"/>
         <source>Display help </source>
-        <translation>Toon help</translation>
+        <translation>Visa hjälp</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="213"/>
         <location filename="ui_mxcodecs.h" line="196"/>
         <source>Help</source>
-        <translation>Help</translation>
+        <translation>Hjälp</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="220"/>
@@ -99,74 +99,74 @@
         <location filename="mxcodecs.cpp" line="106"/>
         <location filename="mxcodecs.cpp" line="113"/>
         <source>&lt;b&gt;Running command...&lt;/b&gt;&lt;p&gt;</source>
-        <translation>&lt;b&gt;Commando uitvoeren...&lt;/b&gt;&lt;p&gt;</translation>
+        <translation>&lt;b&gt;Kör kommando...&lt;/b&gt;&lt;p&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="95"/>
         <location filename="mxcodecs.cpp" line="110"/>
         <source>Cannot connect to the download site</source>
-        <translation>Kan geen verbinding maken met de download site</translation>
+        <translation>Kan inte ansluta till nedladdningssidan</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="101"/>
         <location filename="mxcodecs.cpp" line="116"/>
         <source>Error downloading %1</source>
-        <translation>Download fout %1</translation>
+        <translation>Fel vid nerladdning %1</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="120"/>
         <source>&lt;b&gt;Download Finished.&lt;/b&gt;</source>
-        <translation>Download beëindigd.</translation>
+        <translation>&lt;b&gt;Nerladdning Avslutad.&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="139"/>
         <source>Installing downloaded files</source>
-        <translation>Gedownloade files installeren</translation>
+        <translation>Installerar nerladdade filer</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="144"/>
         <source>No downloaded *.debs files found.</source>
-        <translation>Geen gedownloade *.deb bestanden gevonden.</translation>
+        <translation>Inga nerladdade *.deb filer hittade.</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="151"/>
         <source>&lt;b&gt;Installing...&lt;/b&gt;&lt;p&gt;</source>
-        <translation>Installeren...</translation>
+        <translation>&lt;b&gt;Installerar...&lt;/b&gt;&lt;p&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="154"/>
         <source>Error installing %1</source>
-        <translation>Fout bij installeren %1</translation>
+        <translation>Fel vid installering %1</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="162"/>
         <source>&lt;b&gt;Installation process has finished&lt;/b&gt;</source>
-        <translation>Installatieproces beëindigd</translation>
+        <translation>&lt;b&gt;Installations-processen är avslutad&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="166"/>
         <source>Codecs files have been downloaded and installed successfully.</source>
-        <translation>De codecs bestanden zijn gedownload en succesvol geïnstalleerd.</translation>
+        <translation>Codecs-filerna har laddats ner och framgångsrikt installerats.</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="170"/>
         <source>Process finished. Errors have occurred during the installation.</source>
-        <translation>Proces beëindigd. Er hebben zich fouten voorgedaan tijdens de installatie.</translation>
+        <translation>Processen avslutad. Fel har inträffat under installationen.</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="182"/>
         <source>About mx-codecs</source>
-        <translation>Over mx-codecs</translation>
+        <translation>Om mx-codecs</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="183"/>
         <source>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;&lt;h2&gt;MX Codecs Installer&lt;/h2&gt;&lt;/b&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;MX14+git20140217&lt;/p&gt;&lt;p&gt;&lt;h3&gt;Simple codecs      downloader for antiX MX&lt;/h3&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;http://www.mepiscommunity.org/mx&quot;&gt;      http://www.mepiscommunity.org/mx&lt;/a&gt;&lt;br /&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Copyright (c) antiX&lt;br /&gt;&lt;br /&gt;&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;&lt;h2&gt;MX Codecs Installeerder&lt;/h2&gt;&lt;/b&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;MX14+git20140217&lt;/p&gt;&lt;p&gt;&lt;h3&gt;Eenvoudige codecs downloader voor antiX MX&lt;/h3&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;http://www.mepiscommunity.org/mx&quot;&gt; http://www.mepiscommunity.org/mx&lt;/a&gt;&lt;br /&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Copyright (c) antiX&lt;br /&gt;&lt;br /&gt;&lt;/p&gt;</translation>
+        <translation>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;&lt;h2&gt;MX Codecs Installerare&lt;/h2&gt;&lt;/b&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;MX14+git20140217&lt;/p&gt;&lt;p&gt;&lt;h3&gt;Enkel codecs nerladdare för antiX MX&lt;/h3&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;a href=&quot;http://www.mepiscommunity.org/mx&quot;&gt; http://www.mepiscommunity.org/mx&lt;/a&gt;&lt;br /&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Copyright (c) antiX&lt;br /&gt;&lt;br /&gt;&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.cpp" line="186"/>
         <source>License</source>
-        <translation>Licentie</translation>
+        <translation>Licens</translation>
     </message>
 </context>
 </TS>
