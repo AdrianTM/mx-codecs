20d6bfd19e5d17eea69f409b7f50f23a4a7184a6
oldgizahub <geoffgigg@hotmail.com>
2018-11-25 15:19:59 -0500

    Late completion of Danish

--- translations/mx-codecs_da.ts
+++ translations/mx-codecs_da.ts
@@ -27,7 +27,7 @@
     <message>
         <location filename="mxcodecs.ui" line="27"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;This application allows you to install restricted codecs that permit advanced video and audio functions.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;In some juridictions their distribution may be limited so the user must meet local regulations. &lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Do you assume legal responsibility for downloading these codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;Programmet giver dig mulighed for at installere restriktive codecs der tillader avancerede video- og lydfunktioner.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;Deres distribution kan være begrænset i nogle områder så brugeren skal efterleve lokale bestemmelser. &lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Vil du påtage dig juridisk ansvar for download af disse codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="mxcodecs.ui" line="44"/>
