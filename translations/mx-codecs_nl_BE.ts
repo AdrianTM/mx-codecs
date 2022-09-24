8289eb56cd522a9767eaa1b128df5ce7abee6b95
Adrian <adrian@mxlinux.org>
2022-09-24 12:05:02 -0400

    Update translations

--- translations/mx-codecs_nl_BE.ts
+++ translations/mx-codecs_nl_BE.ts
@@ -4,38 +4,38 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>MX Codecs Installer</source>
-        <translation type="unfinished"/>
+        <translation>MX Codecs Installeerder</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="27"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;This application allows you to install restricted codecs that permit advanced video and audio functions.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;In some jurisdictions, their distribution may be limited. Please check the regulations in your area. &lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Do you assume legal responsibility for downloading these codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;justify&quot;&gt;Deze applicatie staat u toe gelimiteerde codecs te gebruiken die geavanceerde video en audio functies toestaan.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;In sommige jurisdicties kan hun distributie beperkt zijn. Kijk alstublieft de regelgeving in uw gebied na.&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;p align=&quot;justify&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Neemt u de legale verantwoordelijkheid voor het downloaden van deze codecs?&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="44"/>
         <source>Downloading codecs files</source>
-        <translation type="unfinished"/>
+        <translation>Codecs bestanden downloaden</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="94"/>
         <source>Cancel any changes then quit</source>
-        <translation type="unfinished"/>
+        <translation>Iedere verandering ongedaan maken, dan afsluiten</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="97"/>
         <location filename="../mainwindow.cpp" line="268"/>
         <source>Cancel</source>
-        <translation>Cancel</translation>
+        <translation>Ongedaan maken</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="104"/>
         <source>Alt+N</source>
-        <translation type="unfinished"/>
+        <translation>Alt+N</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="117"/>
         <source>Begin codec download and installation</source>
-        <translation type="unfinished"/>
+        <translation>Begin codec download en installatie</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="120"/>
@@ -45,22 +45,22 @@
     <message>
         <location filename="../mainwindow.ui" line="175"/>
         <source>About this application</source>
-        <translation type="unfinished"/>
+        <translation>Over deze applicatie</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="178"/>
         <source>About...</source>
-        <translation type="unfinished"/>
+        <translation>Over...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="185"/>
         <source>Alt+B</source>
-        <translation type="unfinished"/>
+        <translation>Alt+B</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="214"/>
         <source>Display help </source>
-        <translation type="unfinished"/>
+        <translation>Toon help</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="217"/>
@@ -70,7 +70,7 @@
     <message>
         <location filename="../mainwindow.ui" line="224"/>
         <source>Alt+H</source>
-        <translation type="unfinished"/>
+        <translation>Alt+H</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="105"/>
@@ -82,7 +82,7 @@
         <location filename="../mainwindow.cpp" line="153"/>
         <location filename="../mainwindow.cpp" line="161"/>
         <source>&lt;b&gt;Running command...&lt;/b&gt;&lt;p&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;b&gt;Commando uitvoeren...&lt;/b&gt;&lt;p&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="108"/>
@@ -95,7 +95,7 @@
         <location filename="../mainwindow.cpp" line="193"/>
         <location filename="../mainwindow.cpp" line="246"/>
         <source>Error</source>
-        <translation type="unfinished"/>
+        <translation>Foutmelding</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="109"/>
@@ -103,7 +103,7 @@
         <location filename="../mainwindow.cpp" line="141"/>
         <location filename="../mainwindow.cpp" line="158"/>
         <source>Cannot connect to the download site</source>
-        <translation type="unfinished"/>
+        <translation>Kan geen verbinding maken met de download site</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="115"/>
@@ -111,103 +111,103 @@
         <location filename="../mainwindow.cpp" line="147"/>
         <location filename="../mainwindow.cpp" line="165"/>
         <source>Error downloading %1</source>
-        <translation type="unfinished"/>
+        <translation>Download fout %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="170"/>
         <source>&lt;b&gt;Download Finished.&lt;/b&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;b&gt;Download  klaar.&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="189"/>
         <source>Installing downloaded files</source>
-        <translation type="unfinished"/>
+        <translation>Gedownloade bestanden installeren</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="194"/>
         <source>No downloaded *.debs files found.</source>
-        <translation type="unfinished"/>
+        <translation>Geen gedownloade *.deb bestanden gevonden.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="207"/>
         <source>&lt;b&gt;Installing...&lt;/b&gt;&lt;p&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;b&gt;Installeren...&lt;/b&gt;&lt;p&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="220"/>
         <source>Error installing %1</source>
-        <translation type="unfinished"/>
+        <translation>Fout bij  het installeren %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="224"/>
         <source>Fix missing dependencies...</source>
-        <translation type="unfinished"/>
+        <translation>Herstel ontbrekende afhankelijkheden...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="226"/>
         <source>Error running %1 command</source>
-        <translation type="unfinished"/>
+        <translation>Fout met uitvoeren %1 commando</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="238"/>
         <source>&lt;b&gt;Installation process has finished&lt;/b&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;b&gt;Installatieproces is beëindigd&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="242"/>
         <source>Finished</source>
-        <translation type="unfinished"/>
+        <translation>Voltooid</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="243"/>
         <source>Codecs files have been downloaded and installed successfully.</source>
-        <translation type="unfinished"/>
+        <translation>De codecs bestanden zijn gedownload en succesvol geïnstalleerd.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="247"/>
         <source>Process finished. Errors have occurred during the installation.</source>
-        <translation type="unfinished"/>
+        <translation>Proces beëindigd. Er hebben zich fouten voorgedaan tijdens de installatie.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="261"/>
         <source>About MX Codecs</source>
-        <translation type="unfinished"/>
+        <translation>Over MX Codecs</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="262"/>
         <source>MX Codecs</source>
-        <translation type="unfinished"/>
+        <translation>MX Codecs</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="262"/>
         <source>Version: </source>
-        <translation type="unfinished"/>
+        <translation>Versie:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="264"/>
         <source>Simple codecs downloader for MX Linux</source>
-        <translation type="unfinished"/>
+        <translation>Eenvoudige codecs downloader voor MX Linux</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="265"/>
         <source>Copyright (c) MX Linux</source>
-        <translation type="unfinished"/>
+        <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="266"/>
         <source>License</source>
-        <translation type="unfinished"/>
+        <translation>Licentie</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="267"/>
         <location filename="../mainwindow.cpp" line="277"/>
         <source>Changelog</source>
-        <translation type="unfinished"/>
+        <translation>Changelog</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="285"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Sluiten</translation>
     </message>
 </context>
 <context>
@@ -215,17 +215,17 @@
     <message>
         <location filename="../main.cpp" line="53"/>
         <source>Unable to get exclusive lock</source>
-        <translation type="unfinished"/>
+        <translation>Niet in staat om exclusief slot te verkrijgen</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="54"/>
         <source>Another package management application (like Synaptic or apt-get), is already running. Please close that application first</source>
-        <translation type="unfinished"/>
+        <translation>Een ander pakketbeheerprogramma (zoals Synaptic of apt-get) is reeds in gebruik. Sluit aub eerst dat programma</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
         <source>You must run this program as root.</source>
-        <translation type="unfinished"/>
+        <translation>U dient deze toepassing als &apos;root&apos; uit te voeren.</translation>
     </message>
 </context>
 </TS>
